b020b3a2aa29ab0f564ccc1cac91271359b508a3
Ajschlander <Aschlander21@gmail.com>
2019-11-11 12:35:10 -0600

    Added a background. Added logic to check the answer that the user puts in

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,28 @@
-var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+let letter = '';
+let input = '';
+
+function randomLetter() {
+  letter = alphabet[Math.floor(Math.random()*alphabet.length)];
+  console.log(letter);
+}
 
 function storeInput() {
-  var input = document.getElementById('numberGuess').value;
+  input = document.getElementById('numberGuess').value;
   if (input.length > 1) {
-    alert("please enter a single letter")
+    alert("Please enter a single letter.");
   }
-  else {
-    alert(input);
+  else if (input.length == 0) {
+    alert("Please enter a single letter.");
   }
 }
 
-function handleSubmit(str) {
-  alert(str);
+function checkAnswer() {
+  if (letter == input) {
+    document.getElementById('result').innerHTML = "Winner!";
+  }
+  else {
+    document.getElementById('result').innerHTML = "You lost. Guess again";
+  }
 }
